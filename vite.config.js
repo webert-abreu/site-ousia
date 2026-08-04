0da63ab0a2d636906f8c946ef58114164e314b39
webert-abreu <escalademartech@gmail.com>
2026-08-04 18:53:15 -0300

    feat: pagina de links oficial da bio (/links)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        links: resolve(__dirname, 'links/index.html'),
+        criativos: resolve(__dirname, 'criativos/estaticos/modelo-base.html')
+      }
+    }
+  }
 })
